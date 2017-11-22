300fa158a11a8747c233b6a31c27b8b556f99ec7
Jonathan Rosen <jonbutterminecraftian@gmail.com>
2017-11-21 16:51:32 -0800

    added accessing list elements

---
+++ accessing_list_elems.py
@@ -0,0 +1,16 @@
+# ACCESSING LIST ELEMS
+
+# accessing list elements is pretty simple at first but there are lots of ways you can do it
+# to access one element from a list, write the name of the list and the index of the element in square brackets
+# basic access of list elements looks like:
+my_list = [1, 53, 23]
+my_list[2] # equals 23
+
+# the reason my_list[2] equals 23 and not 53 is because list indices start from 0 instead of 1
+# that means that the first element is my_list[0], the second element is my_list[1] and so on
+
+# EXERCISES:
+my_list = [1, 56, 54412, 653, 10e5]
+# put each element of the list into a variable and make sure the code runs
+
+# MORE TO BE ADDED LATER BUT THESE ARE THE IMPORTANT BITS
